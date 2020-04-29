9ed359fa356f00be5a6dc486c442b7e119789131
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-04-29 17:09:13 +0200

    Added lab2

---
+++ lab2.rs
@@ -0,0 +1,17 @@
+// Exercice écrit par N. Matsakis (2016)
+
+pub fn main() {
+    let string = format!("my friend");
+    greet(string.clone());
+    greet(string);
+}
+
+fn greet(name: String) {
+    println!("Hello, {}!", name);
+}
+
+// But #1: Convertir `greet` pour utiliser l'emprunt, et non la possession,
+// pour que le programme s'exécute sans clonage (copie profonde).
+//
+// But #2: Utiliser une sous-tranche (subslice) pour que cela imprime
+//  "Hello, friend" au lieu de "Hello, my friend".
