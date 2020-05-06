c88cc426c2e64161d317e8301d45326831e3cb40
Evrard-Nil Daillet <evrardnild@gmail.com>
2020-05-06 15:57:35 +0200

    finished lab11

--- src/lab11.rs
+++ src/lab11.rs
@@ -9,24 +9,24 @@
 struct Package {
     from: String,
     to: String,
-    weight: f32
+    weight: f32,
 }
 
 impl Package {
     fn new(from: String, to: String, weight: f32) -> Package {
         if weight <= 0.0 {
-            // Something goes here...
+            panic!();
         } else {
-            return Package {from, to, weight};
+            return Package { from, to, weight };
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        return self.from != self.to;
     }
 
-    fn get_fees(&self, cost_per_kg: f32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cost_per_kg: f32) -> f32 {
+        return self.weight * cost_per_kg;
     }
 }
 
@@ -47,7 +47,6 @@ mod tests {
     fn create_international_package() {
         let country_from = String::from("Spain");
         let country_to = String::from("Russia");
-        
         let package = Package::new(country_from, country_to, 1.2);
 
         assert!(package.is_international());
@@ -58,10 +57,8 @@ mod tests {
         let country_from = String::from("Spain");
         let country_to = String::from("Spain");
 
-        let country_fee = ???;
-        
+        let country_fee = 8.0;
         let package = Package::new(country_from, country_to, 22.0);
-        
         assert_eq!(package.get_fees(country_fee), 176.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod lab10;
+mod lab11;
 mod lab9;
 
 fn main() {}
